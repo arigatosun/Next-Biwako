2ef466d8e54ea511074adb32b220e876931b78f1
tsuchiga <t.koushi@arigatosun.com>
2024-10-16 11:57:26 +0900

    アフィリエイト画面

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,51 +1,93 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: "#00A2EF",
-        secondary: "#363331",
-        accent: "#F7F7F7",
-      },
-      aspectRatio: {
-        'w-16': '16',
-        'h-9': '9',
-      },
-      fontFamily: {
-       'shin-go': ['var(--font-shin-go)'],
-      },
-      fontWeight: {
-        light: '300',
-        normal: '400',
-        medium: '500',
-        semibold: '600',
-        bold: '700',
-        black: '900',
-      },
-      fontSize: {
-        'xxs': '0.625rem', // 10px
-        'xs': '0.75rem',   // 12px
-        'sm': '0.875rem',  // 14px
-        'base': '1rem',    // 16px
-        'lg': '1.125rem',  // 18px
-        'xl': '1.25rem',   // 20px
-        '2xl': '1.5rem',   // 24px
-        '3xl': '1.875rem', // 30px
-        '4xl': '2.25rem',  // 36px
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		aspectRatio: {
+  			'w-16': '16',
+  			'h-9': '9'
+  		},
+  		fontFamily: {
+  			'shin-go': ['var(--font-shin-go)']
+  		},
+  		fontWeight: {
+  			light: '300',
+  			normal: '400',
+  			medium: '500',
+  			semibold: '600',
+  			bold: '700',
+  			black: '900'
+  		},
+  		fontSize: {
+  			xxs: '0.625rem',
+  			xs: '0.75rem',
+  			sm: '0.875rem',
+  			base: '1rem',
+  			lg: '1.125rem',
+  			xl: '1.25rem',
+  			'2xl': '1.5rem',
+  			'3xl': '1.875rem',
+  			'4xl': '2.25rem'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
-  ],
+      require("tailwindcss-animate")
+],
 };
 
 export default config;
